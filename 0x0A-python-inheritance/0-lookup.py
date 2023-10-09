5188ea7df71a9d75056b81e21ceea4d6739bcc38
el hihi zakarya <zakarya.elhihi@gmail.com>
2023-10-09 05:22:29 -0700

    task

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/user/bin/python3
+def lookup(obj):
+    """ Function return all attributes and methodes
+
+    Arg: classe object
+    Return:
+          retun attributes and methodes
+          """
+          return (dir(obj))
