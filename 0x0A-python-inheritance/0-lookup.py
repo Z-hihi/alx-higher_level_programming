3fdd562417eddf64727a1266ff1b465409c8be66
el hihi zakarya <zakarya.elhihi@gmail.com>
2023-10-10 01:56:19 -0700

    task

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,16 +1,7 @@
-#!/usr/bin/python3
-    """ Function return all attribute """
+#!/user/bin/python3
+   """classe inherinte"""
 
 
 def lookup(obj):
-    """ Function that returns the list of available attributes
-        and methods of an object
-
-    Args:
-        obj: instance of the class
-
-    Returns:
-        List of attributes
-    """
-
+    """function return all attribute"""
     return dir(obj)
